6b3cc336f4b53ef89bea6fd64f5952bc12412696
pass-inc0me <brosusbrosus967@gmail.com>
2025-12-24 12:37:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ wget https://github.com/ekzhang/bore/releases/download/v0.5.1/bore-v0.5.1-x86_64
 tar -xf bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz
 sudo mv bore /usr/local/bin/
 sudo pkill -9 mumble-server || true
-mumble-server -ini mumble.ini &
+mumble-server -ini murmur.ini &
 bore local 64738 --to bore.pub
